59146980b5572df0258324534010252173a54c81
Adrian Solberg <adrianhsolberg@gmail.com>
2024-01-02 18:39:34 +0100

    Made basic string manipulation for receipts
    Issue: #7

--- core/src/main/java/core/ReceiptScanner.java
+++ core/src/main/java/core/ReceiptScanner.java
@@ -8,10 +8,41 @@ import net.sourceforge.tess4j.TesseractException;
 
 public class ReceiptScanner {
 
-    public static String scanImage(String filename) throws TesseractException {
+    private static String scanImage(String filename) throws TesseractException {
         File imageFile = new File("core/src/main/resources/core/" + filename);
         ITesseract instance = new Tesseract();
         instance.setDatapath("tessdata");
         return instance.doOCR(imageFile);
+    }
+
+    public static Receipt scanReceipt(String filename) throws TesseractException {
+        String receiptText = scanImage(filename);
+        String[] lines = receiptText.split(System.lineSeparator());
+
+        Receipt receipt = new Receipt();
+        for(String line : lines) {
+            String[] splitLine = line.trim().split("\\s+");
+
+            int i = 0;
+            String name = splitLine[i];
+            while (!splitLine[i].matches("-?\\d+(\\.\\d+)?|-?\\d+(\\,\\d+)?") && i != splitLine.length) {
+                name += " " + splitLine[i];
+                i++;
+            }
+
+            float price = 0;
+            String priceStr = splitLine[splitLine.length-1].replace(',', '.');
+            if (priceStr.matches("-?\\d+(\\.\\d+)?")) {
+                price = Float.parseFloat(priceStr);
+            }
+
+            receipt.addItem(name, price);
+        }
+
+        return receipt;
+    }
+
+    public static void main(String args[]) throws Exception {
+        System.out.println(scanReceipt("IMG_3974.png"));
     }
 }
